1b245741e62ba8d647342f1be8161ed56634f21a
Ardy Dwi Krisnanto <ardy.dwik@gmail.com>
2019-10-10 03:20:10 +0700

    perubahan att pd promo dan admin

--- onlineshop_kodig.id_5.8/database/migrations/2019_10_07_045803_create_promo_table.php
+++ onlineshop_kodig.id_5.8/database/migrations/2019_10_07_045803_create_promo_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePromoAttTable extends Migration
+class CreatePromoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreatePromoAttTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('promo_att');
+        Schema::dropIfExists('promo');
     }
 }
--- onlineshop_kodig.id_5.8/database/migrations/2019_10_07_050037_create_admin_table.php
+++ onlineshop_kodig.id_5.8/database/migrations/2019_10_07_050037_create_admin_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAdminAttTable extends Migration
+class CreateAdminTable extends Migration
 {
     /**
      * Run the migrations.
@@ -33,6 +33,6 @@ class CreateAdminAttTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admin_att');
+        Schema::dropIfExists('admin');
     }
 }
